aa2cf968aa8563a77ef9f89b251c1106c93e7f0a
mukhamed-Ali <mailsmorales21@mail.ru>
2023-10-02 11:39:30 +0600

    layout hero and adaptive

--- app/[lang]/components/Hero/HeroSection.jsx
+++ app/[lang]/components/Hero/HeroSection.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import styles from "./HeroSection.module.scss";
+import "../../globals.scss";
 import ImageGet from "../Ui/ImageGet/ImageGet";
 import ImageLanguage from "../Ui/ImageLanguage/ImageLanguage";
 import ImageGetStarted from "../Ui/ImageGetStarted/ImageGetStarted";
@@ -31,24 +32,27 @@ const item = [
 const HeroSection = () => {
   return (
     <div className={styles.customImg}>
-      <div className={styles.overlay}>
-        <div className={styles.overlayB}>
-        <h1 className={styles.heroText}>
-          High-quality software development created for business automation
-        </h1>
-        <ImageGetStarted name={"Get Started"} />
-        </div>
-        <div className={styles.item}>
-          <ul className={styles.links}>
-            {item.map((items) => (
-              <li key={items.id} className={styles.item}>
-                <div className={styles.itemTitle}>{items.title}</div>
-                <div className={styles.itemSub}>{items.subTitle}</div>
-              </li>
-            ))}
-          </ul>
+      <div className="container">
+        <div className={styles.overlay}>
+          <div className={styles.overlayB}>
+            <h1 className={styles.heroText}>
+              High-quality software development created for business automation
+            </h1>
+            {/* <ImageGetStarted name={"Get Started"} /> */}
+          </div>
+          <div className={styles.item}>
+            <ul className={styles.links}>
+              {item.map((items) => (
+                <li key={items.id} className={styles.item}>
+                  <div className={styles.itemTitle}>{items.title}</div>
+                  <div className={styles.itemSub}>{items.subTitle}</div>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
+        {/* <div className={styles.advantages}>Наши преимущества</div> */}
     </div>
   );
 };
