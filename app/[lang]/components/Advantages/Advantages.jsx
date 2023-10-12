f550aa9933665ca32be51ff244ec5eb2e2ff7c12
mukhamed-Ali <mailsmorales21@mail.ru>
2023-10-12 20:27:09 +0600

    add adaptive and fixed style, svg

--- app/[lang]/components/Advantages/Advantages.jsx
+++ app/[lang]/components/Advantages/Advantages.jsx
@@ -1,33 +1,41 @@
-import React from "react";
-// import AdvantagesIcon from "../Ui/Advantages/AdvantagesIcon";
+"use client"
+import React, { useEffect, useState } from "react";
 import { advantagesItem } from "./advantagesItem";
 import styles from "./Advantages.module.scss";
 import Icons from "@/media/img";
+import useWindowSize from "@/hooks/useWindowSize";
 
 const Advantages = ({}) => {
+
+  const windowSize = useWindowSize();
+  const isSmallScreen = windowSize.width <= 1204;
+
   return (
     <div>
       <h2 className={styles.advantagesTitle}>Наши преимущества</h2>
       <div className="container">
-          <Icons.Stroke className={styles.colorStroke}/>
-        {/* <ul className={styles.list}>
-          {advantagesItem.map((item) => (
-            <li className={styles.item} key={item.id}>
-              <div>
-                <Image src={item.Icon} alt="awdawd" />
+        <div className={styles.section}>
+          {advantagesItem.map((item, index) => (
+            <div key={item.id} className={`${styles.sectionBlock} ${index === 0 || index === 3 ? "" : styles.second}` }>
+              {isSmallScreen ? (
+            <Icons.Stroke className={styles.colorStroke} />
+          ) : (
+            <item.stroke key={item.id} className={styles.colorStroke} />
+          )}
+              <div className={styles.content}>
+                <div className={styles.mini}>
+                  <div><item.icon className={styles.smallIcon} /></div>
+                  <h4 className={styles.title}>
+                    {item.title}
+                  </h4>
+                </div>
+                <p className={styles.titleSub}>
+                  {item.subTitle}
+                </p>
               </div>
-              <h4 className={styles.title}>{item.title}</h4>
-              <p className={styles.subTitle}>{item.subTitle}</p>
-              <div>
-              <Image
-                className={styles.itemStroke}
-                src={item.stroke}
-                alt="awdaw"
-              />
-              </div>
-            </li>
+            </div>
           ))}
-        </ul> */}
+        </div>
       </div>
     </div>
   );
