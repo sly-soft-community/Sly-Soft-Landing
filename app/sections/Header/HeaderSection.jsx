986a04273e64c581e2fa47169fb5599288ac094f
mukhamed-Ali <mailsmorales21@mail.ru>
2023-12-25 22:05:49 +0600

    made the header fixed

--- app/sections/Header/HeaderSection.jsx
+++ app/sections/Header/HeaderSection.jsx
@@ -2,10 +2,30 @@
 
 import styles from "@/sections/Header/HeaderSection.module.scss";
 import NavBar from "@/components/NavBar/NavBar";
+import { useEffect, useRef, useState } from "react";
 
 function HeaderSection() {
+  let [lastScroll, setLastScroll] = useState(0);
+  const headerRef = useRef(null);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      const currentScroll = document.documentElement.scrollTop;
+      if (currentScroll > lastScroll) {
+        headerRef.current.style.top = `-${headerRef.current.offsetHeight}px`;
+      } else {
+        headerRef.current.style.top = "0";
+      }
+      setLastScroll(currentScroll <= 0 ? 0 : currentScroll);
+      // lastScroll = currentScroll <= 0 ? 0 : currentScroll;
+    });
+    // console.log("wdawdawwa");
+  }, [lastScroll]);
+  useEffect(() => {
+    document.body.style.paddingTop = `${headerRef.current.offsetHeight}px`;
+  }, []);
   return (
-    <header className={styles.header}>
+    <header ref={headerRef} className={styles.header}>
       <div className="container">
         <NavBar />
       </div>
